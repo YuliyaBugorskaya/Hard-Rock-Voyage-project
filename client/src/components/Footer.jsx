faa92778c4dff3aff5945e464c82816088a21e13
Eugene <zhenya.ostanko@yandex.ru>
2022-12-23 14:05:37 +0300

    release 1

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import {
+  AppBar, Box, Toolbar, Typography, Link, Button,
+} from '@mui/material';
+import { NavLink } from 'react-router-dom';
 
-export default function Footer() {
+export default function NavBar() {
   return (
-    <div>Footer</div>
+    <Box sx={{
+      flexGrow: 1, position: 'fixed', bottom: 0, left: 0, right: 0, backgroundColor: 'red',
+    }}
+    >
+      <AppBar position="static" sx={{ flexGrow: 1 }}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <NavLink to="/">
+              <Link href="/" underline="none">
+                <Button variant="text" sx={{ color: 'white' }}>Главная страница</Button>
+              </Link>
+            </NavLink>
+
+          </Typography>
+          <NavLink to="/">
+            <Link href="/" underline="none">
+              <Button variant="text" sx={{ color: 'white' }}>О нас</Button>
+            </Link>
+          </NavLink>
+          <NavLink to="/">
+            <Link href="/" underline="none">
+              <Button variant="text" sx={{ color: 'white' }}>Контакты</Button>
+            </Link>
+          </NavLink>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
--- client/src/components/SignIn/SignIn.jsx
+++ client/src/components/SignIn/SignIn.jsx
@@ -1,7 +1,53 @@
 import React from 'react';
+import {
+  Box, Button, FormControl, TextField, Typography,
+} from '@mui/material';
+import { useDispatch } from 'react-redux';
+import { signinUser } from '../../redux/EugeneSlices/userSlice';
 
-export default function SignIn() {
+export default function Login() {
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(signinUser(Object.fromEntries(new FormData(e.target))));
+    window.location = '/';
+  };
   return (
-    <div>SignIn</div>
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      minHeight="80vh"
+    >
+      <form onSubmit={submitHandler}>
+        <FormControl>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Login
+          </Typography>
+          <TextField
+            name="email"
+            required
+            id="outlined-required"
+            label="Email"
+            type="email"
+          />
+          <TextField
+            name="password"
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+          />
+          <Button variant="contained">Sign In</Button>
+        </FormControl>
+      </form>
+
+    </Box>
   );
 }
