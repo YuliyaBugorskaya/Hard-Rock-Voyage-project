f219775001ff4b8d283f64b83517963e6854ada0
Yuliya <galichina@mail.ru>
2023-01-10 16:10:50 +0300

    AllEv NewEv WO Comments

--- client/src/components/AllEvents/AllEvents.jsx
+++ client/src/components/AllEvents/AllEvents.jsx
@@ -15,10 +15,8 @@ export default function AllEvents() {
   const dispatch = useDispatch();
   const allEvents = useSelector((state) => state.allEvents);
   const [input, setInput] = useState('');
-  // const [isFilter, setIsFilter] = useState(true);
 
   const [page, setPage] = useState(1);
-  // console.log(allEvents, 'allEvents--->>>>>');
 
   useEffect(() => {
     if (input === 'All') {
@@ -34,25 +32,18 @@ export default function AllEvents() {
   };
 
   const dateChange = (event) => {
-    // console.log(event.target.value, '');
     setInput(event.target.value);
-    // if (input === 'All') {
-    //   setInput(false);
-    //   // dispatch(setFilterData(event.target.value));
-    // }
     dispatch(setFilterData(event.target.value));
-    // setIsFilter(false);
   };
-  // console.log(input, '--->');
+
   return (
     <Box
       className="allEvents"
       component="form"
       sx={{
         '& .MuiTextField-root': { m: 0.4, width: '55ch' },
-        // backgroundImage: `url(${'../css/images/_7Fr1kwBRRM.jpeg'})`,
+        backgroundImage: `url(${'../css/images/_7Fr1kwBRRM.jpeg'})`,
         backgroundRepeat: 'no-repeat',
-        // background-repeat: 'no-repeat',
       }}
       display="flex"
       noValidate
@@ -64,8 +55,6 @@ export default function AllEvents() {
       <div>
         <CssBaseline />
         <Container maxWidth={false} disableGutters>
-
-          {/* <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} /> */}
           <div>
             <Box sx={{ minWidth: 120 }}>
               <FormControl fullWidth sx={{ marginTop: '10px' }}>
@@ -116,101 +105,3 @@ export default function AllEvents() {
     </Box>
   );
 }
-
-// export default function AllEvents() {
-//   const dispatch = useDispatch();
-//   const allEvents = useSelector((state) => state.allEvents);
-//   const [input, setInput] = useState('');
-//   const [isFilter, setIsFilter] = useState(true);
-
-//   const [page, setPage] = useState(1);
-
-//   useEffect(() => {
-//     dispatch(getAllEvents({ page }));
-//   }, [page]);
-
-//   const navigate = useNavigate();
-//   const changeHandler = () => {
-//     navigate('/newEvent');
-//   };
-//   const handleChange = (event) => {
-//     // console.log(event, 'event---->');
-//     setInput(event.target.value);
-//     dispatch(setFilterData(event));
-//     // setIsFilter(false);
-//   };
-//   const itemHandler = () => {
-//     setIsFilter(false);
-//   };
-//   const allHandler = () => {
-//     setIsFilter(true);
-//   };
-//   console.log('isFilter', isFilter);
-//   return (
-//     <Box
-//       className="allEvents"
-//       component="form"
-//       sx={{
-//         '& .MuiTextField-root': { m: 0.4, width: '55ch' },
-//         backgroundImage: `url(${'../css/images/_7Fr1kwBRRM.jpeg'})`,
-//       }}
-//       display="flex"
-//       noValidate
-//       autoComplete="off"
-//       alignItems="center"
-//       justifyContent="center"
-//       minHeight="80vh"
-//     >
-//       <div>
-//         <CssBaseline />
-//         <Container maxWidth={false} disableGutters>
-
-//           {/* <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} /> */}
-//           <div>
-//             <Box sx={{ minWidth: 120 }}>
-//               <FormControl fullWidth sx={{ marginTop: '10px' }}>
-//                 <InputLabel id="demo-simple-select-label">Дата</InputLabel>
-//                 <Select
-//                   labelId="demo-simple-select-label"
-//                   id="demo-simple-select"
-//                   onChange={handleChange}
-//                   value={input}
-//                   label="дата"
-//                 >
-//                   {allEvents?.map((el) => (<MenuItem onClick={itemHandler} value={el.startDate}>{el.startDate}</MenuItem>
-
-//                   ))}
-//                   <MenuItem onClick={allHandler} value="All">All</MenuItem>
-//                 </Select>
-//               </FormControl>
-//             </Box>
-//             <Button sx={{ width: '100%', maxWidth: 360, marginTop: '10px' }} onClick={changeHandler} variant="contained" color="secondary">
-//               Создать событие
-//             </Button>
-//             <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-
-//               {allEvents?.filter((el) => isFilter || el.startDate === input).map((el) => (
-//               // .
-//                 <EventCard key={el.id} oneEventCard={el} />
-//               ))}
-//             </List>
-//           </div>
-//           <div>
-//             <Stack spacing={2}>
-//               <Pagination
-//                 page={Number(page)}
-//                 count={3}
-//                 renderItem={(item) => (
-//                   <PaginationItem
-//                     slots={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
-//                     {...item}
-//                   />
-//                 )}
-//               />
-//             </Stack>
-//           </div>
-//         </Container>
-//       </div>
-//     </Box>
-//   );
-// }
--- client/src/components/CreateEvent/CreateEvent.jsx
+++ client/src/components/CreateEvent/CreateEvent.jsx
@@ -1,20 +1,16 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import {
   Row, Col, Form, Container,
 } from 'reactstrap';
 import {
   Button, FormControl, TextField, Typography,
 } from '@mui/material';
-import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-// import { submitEvent } from '../../redux/YuliyaSlices/allEventsSlice';
-// import eventFoto from '`url(${'../css/images/_7Fr1kwBRRM.jpeg'})`';
 
 export default function CreateEvent() {
   // стейт для текущего события и стейт для картинки, которую загружаем из инпута
   const [img, setImg] = useState(null);
-  // const [fotoFromVoyage, setFotoFromVoyage] = useState(null);
   const [input, setInput] = useState({
     title: '',
     description: '',
@@ -23,7 +19,6 @@ export default function CreateEvent() {
     finishDate: '',
     startPoint: '',
     finishPoint: '',
-    // image: '',
   });
 
   const navigate = useNavigate();
@@ -31,7 +26,6 @@ export default function CreateEvent() {
   const inputHandler = (e) => {
     setInput((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
-  // const dispatch = useDispatch();
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -49,11 +43,7 @@ export default function CreateEvent() {
         'content-type': 'multipart/form-data',
       },
     })
-      // .then((res) => console.log('******res.data**********', res.data, res.data.path))
-      // .then((res) => setFotoFromVoyage(res.data.path))
       .then((res) => (res.data));
-    // .then((res) => dispatch(submitEvent(res.data)));
-    // .then((res) => console.log(submitEvent(res.data.input, 'res.data.input----->')));
     setInput({});
     navigate('/lk');
   };
@@ -151,7 +141,6 @@ export default function CreateEvent() {
                   value={input.image || ''}
                   onChange={inputHandler}
                 /> */}
-                {/* <Button style={{ marginBottom: '20px' }} variant="contained">Загрузить фото</Button> */}
                 <Typography variant="h10" component="h5" sx={{ flexGrow: 1 }}>
                   Добавь фото к событию
                 </Typography>
@@ -172,287 +161,3 @@ export default function CreateEvent() {
     </div>
   );
 }
-
-// version 08/01/23
-
-// import React, { useCallback, useState } from 'react';
-// import {
-//   Row, Col, Form, Container,
-// } from 'reactstrap';
-// import {
-//   Button, FormControl, TextField, Typography,
-// } from '@mui/material';
-// import { useDispatch } from 'react-redux';
-// import { useNavigate } from 'react-router-dom';
-// import axios from 'axios';
-// import { submitEvent } from '../../redux/YuliyaSlices/allEventsSlice';
-// // import eventFoto from '`url(${'../css/images/_7Fr1kwBRRM.jpeg'})`';
-
-// export default function CreateEvent() {
-//   // стейт для текущего события и тейт для картинки, которую загружаем из инпута
-//   const [img, setImg] = useState(null);
-//   const [fotoFromVoyage, setFotoFromVoyage] = useState(null);
-//   const [input, setInput] = useState({
-//     title: '',
-//     description: '',
-//     fullDescription: '',
-//     startDate: '',
-//     finishDate: '',
-//     startPoint: '',
-//     finishPoint: '',
-//     image: '',
-//   });
-
-//   const navigate = useNavigate();
-
-//   const inputHandler = (e) => {
-//     setInput((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-//   };
-//   const dispatch = useDispatch();
-
-//   const submitHandler = (e) => {
-//     e.preventDefault();
-//     dispatch(submitEvent(input));
-//     setInput({});
-//     navigate('/lk');
-//   };
-
-//   const sendFile = useCallback(async () => {
-//     try {
-//       const data = new FormData();
-//       data.append('fotoFromVoyage', img);
-//       await axios.post('/upload/foto', data, {
-//         headers: {
-//           'content-type': 'multipart/form-data',
-//         },
-//       })
-//         // .then((res) => console.log(res.data.path));
-//         .then((res) => setFotoFromVoyage(res.data.path));
-//       // console.log(res.data.path, '=========>>>');
-//     } catch (error) {}
-//   }, [img]);
-
-//   return (
-//     <div>
-//       <Container style={{
-//         marginLeft: '400px',
-//         display: 'flex',
-//       }}
-//       >
-//         <Row className="formRow">
-//           <Col className="formCol">
-//             <Form onSubmit={submitHandler}>
-//               <FormControl
-//                 sx={{
-//                   '& .MuiTextField-root': { m: 0.4, width: '55ch' },
-//                 }}
-//               >
-//                 <Typography variant="h6" component="h2" sx={{ flexGrow: 1 }}>
-//                   Создай свое событие
-//                 </Typography>
-//                 <TextField
-//                   name="title"
-//                   required
-//                   id="outlined-required"
-//                   label="Title"
-//                   type="text"
-//                   value={input.title || ''}
-//                   onChange={inputHandler}
-//                 />
-//                 <TextField
-//                   name="description"
-//                   required
-//                   id="outlined-required"
-//                   label="Short Description"
-//                   type="text"
-//                   value={input.description || ''}
-//                   onChange={inputHandler}
-//                 />
-//                 <TextField
-//                   required
-//                   name="fullDescription"
-//                   id="outlined-input"
-//                   label="Full Description"
-//                   type="text"
-//                   value={input.fullDescription || ''}
-//                   onChange={inputHandler}
-//                 />
-//                 <Typography variant="h10" component="h5" sx={{ flexGrow: 1 }}>
-//                   Дата старта поездки
-//                 </Typography>
-//                 <TextField
-//                   required
-//                   name="startDate"
-//                   id="outlined-input"
-//                   type="date"
-//                   value={input.startDate || ''}
-//                   onChange={inputHandler}
-//                 />
-//                 <Typography variant="h10" component="h5" sx={{ flexGrow: 1 }}>
-//                   Дата завершения поездки
-//                 </Typography>
-//                 <TextField
-//                   required
-//                   name="finishDate"
-//                   id="outlined-input"
-//                   type="date"
-//                   value={input.finishDate || ''}
-//                   onChange={inputHandler}
-//                 />
-//                 <TextField
-//                   required
-//                   name="startPoint"
-//                   id="outlined-input"
-//                   label="Start Point"
-//                   type="text"
-//                   value={input.startPoint || ''}
-//                   onChange={inputHandler}
-//                 />
-//                 <TextField
-//                   required
-//                   name="finishPoint"
-//                   id="outlined-input"
-//                   label="Finish Point"
-//                   type="text"
-//                   value={input.finishPoint || ''}
-//                   onChange={inputHandler}
-//                 />
-//                 {/* <TextField
-//                   required
-//                   name="image"
-//                   id="outlined-input"
-//                   label="image"
-//                   type="text"
-//                   value={input.image || ''}
-//                   onChange={inputHandler}
-//                 /> */}
-//                 {/* <Button style={{ marginBottom: '20px' }} variant="contained">Загрузить фото</Button> */}
-//                 <Typography variant="h10" component="h5" sx={{ flexGrow: 1 }}>
-//                   Добавь фото к событию
-//                 </Typography>
-//                 <input type="file" onChange={(e) => setImg(e.target.files[0])} />
-//                 <Button type="submit" variant="contained">Создать событие</Button>
-//               </FormControl>
-//             </Form>
-//           </Col>
-//         </Row>
-//       </Container>
-//       <div className="fotoFromVoyage">
-//         {
-//           fotoFromVoyage
-//             ? <img className="logo" src={`${fotoFromVoyage}`} alt="fotoFromVoyage" />
-//             : <img className="logo" src="/css/images/_7Fr1kwBRRM.jpeg" alt="fotoFromVoyage" />
-//         }
-
-//       </div>
-//       <input type="file" onChange={(e) => setImg(e.target.files[0])} />
-//       <button type="submit" className="btn" onClick={sendFile}>Добавить фото</button>
-//     </div>
-//   );
-// }
-
-// version 2022
-/* <Box
-component="form"
-sx={{
-  '& .MuiTextField-root': { m: 0.4, width: '55ch' },
-}}
-display="flex"
-noValidate
-autoComplete="off"
-alignItems="center"
-justifyContent="center"
-minHeight="80vh"
->
-<Form onSubmit={submitHandler}>
-  <FormControl>
-    <Typography variant="h6" component="h2" sx={{ flexGrow: 1 }}>
-      Создай свое событие
-    </Typography>
-    <TextField
-      name="title"
-      required
-      id="outlined-required"
-      label="Title"
-      type="text"
-      value={input.title || ''}
-      onChange={inputHandler}
-    />
-    <TextField
-      name="description"
-      required
-      id="outlined-required"
-      label="Short Description"
-      type="text"
-      value={input.description || ''}
-      onChange={inputHandler}
-    />
-    <TextField
-      required
-      name="fullDescription"
-      id="outlined-input"
-      label="Full Description"
-      type="text"
-      value={input.fullDescription || ''}
-      onChange={inputHandler}
-    />
-    <Typography variant="h10" component="h5" sx={{ flexGrow: 1 }}>
-      Дата старта поездки
-    </Typography>
-    <TextField
-      required
-      name="startDate"
-      id="outlined-input"
-      // label="Start Date"
-      type="date"
-      value={input.startDate || ''}
-      onChange={inputHandler}
-    />
-    <Typography variant="h10" component="h5" sx={{ flexGrow: 1 }}>
-      Дата завершения поездки
-    </Typography>
-    <TextField
-      required
-      name="finishDate"
-      id="outlined-input"
-      // label="Finish Date"
-      type="date"
-      value={input.finishDate || ''}
-      onChange={inputHandler}
-    />
-    <TextField
-      required
-      name="startPoint"
-      id="outlined-input"
-      label="Start Point"
-      type="text"
-      value={input.startPoint || ''}
-      onChange={inputHandler}
-    />
-    <TextField
-      required
-      name="finishPoint"
-      id="outlined-input"
-      label="Finish Point"
-      type="text"
-      value={input.finishPoint || ''}
-      onChange={inputHandler}
-    />
-    <TextField
-      required
-      name="image"
-      id="outlined-input"
-      label="image"
-      type="text"
-      value={input.image || ''}
-      onChange={inputHandler}
-    />
-    <Button style={{ marginBottom: '20px' }} variant="contained">Загрузить фото</Button>
-
-  </FormControl>
-  <Button type="submit" variant="contained">Создать событие</Button>
-</Form>
-
-</Box>
-);
-} */
